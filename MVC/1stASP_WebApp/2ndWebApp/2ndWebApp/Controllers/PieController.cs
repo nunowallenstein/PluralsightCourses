d2bc356e2304dbed034fea2759f2d1a31cef647d
nuno <myemail@test.com>
2022-04-19 08:54:21 +0100

    Adicionado repositorio remote

---
+++ MVC/1stASP_WebApp/2ndWebApp/2ndWebApp/Controllers/PieController.cs
@@ -0,0 +1,27 @@
+﻿using _2ndWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2ndWebApp.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+            
+        }
+        public ViewResult List()
+        {
+            ViewBag.CurrentCategory = "Cheese Cake";
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
